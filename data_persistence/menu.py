44c22be4693a70bbc38b741344c52c95db89ec2f
Fernanda Castillo <fernandaaracelicastillo@gmail.com>
2023-06-22 00:39:48 -0300

    se crean los ultimos metodos del menu

--- data_persistence/menu.py
+++ data_persistence/menu.py
@@ -21,3 +21,47 @@ class Menu:
 
     def get_law_by_keyword(self, keyword):
         self.database.get_law_by_keyword(keyword) 
+
+    def register_law(self):
+        normative_type = input('Selecciona el n° correspondiente al tipo de normativa:\n1) Ley\n2) Decreto\n3) Resolución\n')
+        normative_num = input('Ingresa el número de la normativa: ')
+        date = input('Ingresa la fecha en formato AAAA-MM-DD: ')
+        description = input('Ingresa una breve descripción: ')
+        category = input('Selecciona el n° correspondiente a la categoría:\n1) Laboral\n2) Penal\n3) Civil\n4) Comercial\n5) Familia y sucesiones\n6) Agrario y ambiental\n7) Minería\n8) Derecho informático\n')
+        jurisdiction = input('Selecciona el n° correspondiente a la jurisdicción:\n1) Nacional\n2) Provincial\n')
+        keywords = input('Ingresa las palabras clave separadas por coma: ')
+        print(self.database.add_law(normative_num, date, keywords, description, category, jurisdiction, normative_type))
+
+    def update_law(self):
+        register_num = input('Ingresa el número de registro de la ley: ')
+        description = input('Ingresa la nueva descripción: ')
+        print(self.database.update_law_by_register_num(register_num, description))
+
+    def delete_law(self):
+        register_num = input('Ingresa el número de registro de la ley: ')
+        print(self.database.delete_law_by_register_num(register_num))
+
+    def run(self):
+        while True:
+            self.show_menu()
+            option = input("Elegir una opción: ")
+
+            if option == '1':
+                self.get_laws()
+            elif option == '2':
+                law_num = input('Ingresa el número de la ley: ')
+                self.get_law_by_num(law_num)
+            elif option == '3':
+                keyword = input('Ingresa la palabra clave: ')
+                self.get_law_by_keyword(keyword)
+            elif option == '4':
+                self.register_law()
+            elif option == '5':
+                self.update_law()
+            elif option == '6':
+                self.delete_law()
+            elif option == '7':
+                print('¡Gracias por utilizar nuestros servicios!')
+                break
+            else:
+                print('Opción no válida ¡Vuelva a intentarlo!') 
--- main.py
+++ main.py
@@ -1,57 +1,10 @@
 from data_persistence.database import Database
 from config.db_config import mydb
-import utils.format_data as format_data
+from data_persistence.menu import Menu
 
+# Instancio una nueva DB con la config correspondiente
 new_db = Database(mydb)
 
-def menuPrincipal():
-    aContinuar = True
-    while(aContinuar):
-        opcionCorrecta = False
-        while(not opcionCorrecta):
-            print("########################### MENÚ ###########################")
-            print("1.- Traer Leyes")
-            print("2.- Traer Ley por N° de Registro")
-            print("3.- Traer Ley por palabra clave")
-            print("4.- Registrar Ley")
-            print("5.- Actualizar Ley")
-            print("6.- Borrar Ley")
-            print("7.- Salir")
-            print("#############################################################")
-
-            opcion = int(input("Elegir una opción: "))
-
-            if opcion < 1 or opcion > 7:
-                print("Incorrect choice. Choose a different one")
-            elif opcion == 7:
-                aContinuar = False
-                print("¡Gracias por utilizar nuestros servicios!")
-                break
-            else:
-                opcionCorrecta = True
-                ejecutarOpcion(opcion)
-
-def ejecutarOpcion(opcion):
-    if opcion == 1:
-        try:
-            leyes = new_db.get_all_laws()
-            if len(leyes) > 0:
-                format_data.format_data(leyes)
-            else:
-                print("No hay Leyes")
-        except:
-             print("Hubo un error")
-    elif opcion == 2:
-       print("Hubo un error")
-    elif opcion == 3:
-        print("Hubo un error")
-    elif opcion == 4:
-        print("Registrar Leyes")
-    elif opcion == 5:
-        print("Actualizar Ley")
-    elif opcion == 6:
-        print("Borrar Ley")
-    else:
-        print("¡Lo sentimos, vuelva a intentarlo!")
-
-menuPrincipal()
+# Instancio y ejecuto el menú 
+app_menu = Menu(new_db)
+app_menu.run()
